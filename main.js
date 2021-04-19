4f484e8fad1555776f5bee72a1f33e14e1dc6866
Dhruv Gour <73885735+Dhruv-gour@users.noreply.github.com>
2021-04-19 16:44:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -57,10 +57,44 @@ function gotPoses(results)
 function draw() {
 	image(video, 0, 0, 600, 500);
 	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+	fill("#FF0000");
+	stroke("#FF0000");
+
+	if(scoreRightWrist > 0.2)
+	{ 
+		circle(rightWristX,rightWristY,20);
+
+			song2.stop();
+
+		if(song1_status == false)
+		{
+			song1.play();
+			document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song"
+		}
+	}
+
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+
+			song1.stop();
+
+		if(song2_status == false)
+		{
+			song2.play();
+			document.getElementById("song").innerHTML = "Playing - Peter Pan Song"
+		}
+	}
+
 }
+
 function play()
 {
 	song.play();
 	song.setVolume(1);
 	song.rate(1);
 }
+
